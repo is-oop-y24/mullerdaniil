3b954140ca7a1c0f3675c8181aede43d2b563200
Daniil Muller <mullerdaniil@mail.ru>
2021-09-13 23:41:04 +0300

    chore: add lab-1

---
+++ Shops/Models/ProductInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace Shops.Models
+{
+    public class ProductInfo
+    {
+        public ProductInfo(string name, double price, int count)
+        {
+            Name = name;
+            Price = price;
+            Count = count;
+        }
+
+        public string Name { get; }
+        public double Price { get; }
+        public int Count { get; }
+    }
+}
---
+++ Shops/Models/Shop.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Shops.Models
+{
+    public class Shop
+    {
+        private ProductRegister _productRegister;
+        internal Shop(string name, string address, int id, ProductRegister productRegister)
+        {
+            Name = name;
+            Address = address;
+            Id = id;
+            _productRegister = productRegister;
+        }
+
+        public string Name { get; }
+        public string Address { get; }
+        public int Id { get; }
+
+        public bool AddProducts(Product product, int count, double price)
+        {
+            return _productRegister.AddProduct(new ShopItem(product, count, price), this);
+        }
+
+        public bool Buy(Person person, Product product, int count)
+        {
+            ShopItem shopItem = _productRegister.FindShopItem(product, this);
+
+            if (shopItem == null)
+            {
+                Console.Error.WriteLine("Can not buy the product. No such product in the shop.");
+                return false;
+            }
+
+            double expectedMoney = shopItem.Price * count;
+            double actualMoney = person.Money;
+
+            if (count > shopItem.Count)
+            {
+                Console.Error.WriteLine("Can not buy the product. Not enough product in the shop.");
+            }
+
+            if (actualMoney >= expectedMoney)
+            {
+                person.WithdrawMoney(expectedMoney);
+                _productRegister.TakeProduct(product, count, this);
+                return true;
+            }
+            else
+            {
+                Console.Error.WriteLine("Can not buy the product. Not enough money.");
+                return false;
+            }
+        }
+
+        public ProductInfo GetProductInfo(Product product)
+        {
+            ShopItem item = _productRegister.FindShopItem(product, this);
+            return new ProductInfo(item.Product.Name, item.Price, item.Count);
+        }
+
+        public void ChangePrice(Product product, double newPrice)
+        {
+            ShopItem item = _productRegister.FindShopItem(product, this);
+            item.Price = newPrice;
+        }
+    }
+}
